c3e168f204fcd85365446c402d57acd2815a1aa1
hoangtv2506 <96108978+hoangtv2506@users.noreply.github.com>
2022-08-05 14:26:58 +0700

    feat: home page UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 import { Box } from "@mui/material";
+import HomeView from "./views/pages/HomeView";
 function App() {
   return (
     <Box>
       <HeaderComponent />
+      <HomeView />
       <FooterComponent />
     </Box>
   );
